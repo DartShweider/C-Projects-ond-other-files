bf16af4861b00edd931437c6eabc0715f0a175fa
DartShweider <34719599+DartShweider@users.noreply.github.com>
2018-09-01 20:34:45 +0300

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -6,6 +6,8 @@ int main()
 {
         
         float a, b, c;
+        float conditional_zero = 0.00001;
+        
         std::cout<< "Уравнение вида ax^2 + bx + c = 0. Введите параметры a, b, c. " << std::endl;
         std::cout<< "Параметр а: ";
         std::cin >> a;
@@ -14,17 +16,51 @@ int main()
         std::cout<< "Параметр c: ";
         std::cin >> c;
         
-        float d = pow(b, 2) - 4*a*c;
-        std::cout <<"Дискриминант: " << d << std::endl;
-        if (d > 0.00001) 
+        
+        if (abs(a) > conditional_zero and abs(b) > conditional_zero and abs(c) > conditional_zero)
         {
-            std::cout<< "Корень_1: "<< (-b - sqrt(d))/(2*a)<< "  " << "Корень_2: " << (-b + sqrt(d))/(2*a) << std::endl;
-        } else if (abs(d) <= 0.00001) 
+            float d = pow(b, 2) - 4*a*c;
+            std::cout <<"Дискриминант: " << d << std::endl;
+            if (d > conditional_zero) 
+            {
+                std::cout<< "Корень_1: "<< (-b - sqrt(d))/(2*a)<< "  " << "Корень_2: " << (-b + sqrt(d))/(2*a) << std::endl;
+            } else if (abs(d) <= conditional_zero) 
+            {
+                std::cout<< "Корень: "<< -b/2 << std::endl;
+            } else if (d < -conditional_zero)
+            {
+                std::cout<< "Действительных корней нет" << std::endl;
+            }
+            
+        } else if (abs(a) < conditional_zero) 
         {
-            std::cout<< "Корень: "<< -b/2 << std::endl;
-        } else if (d < -0.00001)
+            if (abs(b) > conditional_zero)
+            {
+                std::cout<< "Корень: "<< -c/b << std::endl;
+            } else 
+            {
+                std::cout<< " Действительных корней нет" << std::endl;
+            }
+            
+            
+        }  else if (abs(b) < conditional_zero) 
         {
-            std::cout<< "Действительных корней нет" << std::endl;
+            if (abs(a) > conditional_zero)
+            {
+                std::cout<<"Корень_1: "<< -sqrt(c/a)<< "  " << "Корень_2: " << sqrt(c/a) << std::endl;
+            } else 
+            {
+                std::cout<< " Действительных корней нет" << std::endl;
+            }
+        } else if (abs(c) < conditional_zero) 
+        {
+            if (abs(a) > conditional_zero)
+            {
+                std::cout<< "Корень_1: "<< -b/a << "Корень_2: "<< 0 << std::endl;
+            } else 
+            {
+                std::cout << " Действительных корней нет" << std::endl;
+            }
         }
         return 0;
 }
