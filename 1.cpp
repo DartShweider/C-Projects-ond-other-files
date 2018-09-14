4dacc8b96d15e359afb3f9f6f9d62a09298e6df6
Dyomkin Dmitry <34719599+DartShweider@users.noreply.github.com>
2018-09-14 23:58:51 +0300

    Add files via upload
    Исправил косяк в функции решения уравнений, создал структуру, хранящую всю информацию для решения уравнений, использовал указатели, закомментировал код.

--- 1.cpp
+++ 1.cpp
@@ -1,88 +1,96 @@
 #include <iostream>
 #include <stdio.h>
 #include <math.h>
+#include <string>
 
-void inputData(float* inputDataArray)
+struct equationData             //Объявление структуры, содержащей данные, необходимые для  решения уравнения. После создания экземпляра структуры, мы будем изменять его данные по     ходу решения, используя указатели//
 {
-    float a, b, c;
     
+    int numberOfRoots = 0;      //Объявление и инициализация переменных: кол-во корней, коэффициенты уравнения, массив с корнями уравнения//
+    float a = 0;
+    float b = 0;
+    float c = 0;
+    float roots[2] = {0, 0};
+    
+};
+
+void inputData(equationData* eqData)        //Функция ввода данных//
+{
         
     std::cout<< "Уравнение вида ax^2 + bx + c = 0. Введите параметры a, b, c. " << std::endl;
     std::cout<< "Параметр а: ";
-    std::cin >> a;
+    std::cin >> eqData->a;                  //Ввод коэффициентов "a", "b", "c"//
     std::cout<< "Параметр b: ";
-    std::cin >> b;
+    std::cin >> eqData->b;
     std::cout<< "Параметр c: ";
-    std::cin >> c;
-    inputDataArray[0] = a;
-    inputDataArray[1] = b;
-    inputDataArray[2] = c;
+    std::cin >> eqData->c;
+
 }
 
-void solutionOfEquation(float* inputDataArray, float* rootsInformation)
+void solutionOfEquation(equationData* eqData)       //Функция решения уравнения//
 {
-    float conditional_zero = 0.00001;
-    int numberOfRoots = 0;
-    float a = inputDataArray[0];
-    float b = inputDataArray[1];
-    float c = inputDataArray[2];
-        
-        if (abs(a) > conditional_zero and abs(b) > conditional_zero and abs(c) > conditional_zero)
+    
+    float conditional_zero = 0.00001;       //Условный нуль - значение, с которым мы сравниваем модуль числа//
+    float a = eqData->a;                    //Извлекаем коэффициент "а" из структуры для более удобной работы//
+    float b = eqData->b;                    //Извлекаем коэффициент "b" из структуры для более удобной работы//
+    float c = eqData->c;                    //Извлекаем коэффициент "с" из структуры для более удобной работы//
+    
+    // А далее рассамтриваем разные случаи квадратного уравнения и решаем их//
+    
+        if (abs(a) > conditional_zero)      
         {
+            
             float d = pow(b, 2) - 4*a*c;
             
             if (d > conditional_zero) 
             {
-                rootsInformation[0] = (-b - sqrt(d))/(2*a);
-                rootsInformation[1] = (-b + sqrt(d))/(2*a);
-                numberOfRoots = 2;
+                eqData->roots[0] = (-b - sqrt(d))/(2*a);
+                eqData->roots[1] = (-b + sqrt(d))/(2*a);
+                eqData->numberOfRoots = 2;
                 
             } else if (abs(d) <= conditional_zero) 
             {
-                rootsInformation[0] = -b/2;
-                numberOfRoots = 1;
+                eqData->roots[0] = -b/2;
+                eqData->numberOfRoots = 1;
                 
             } else if (d < -conditional_zero)
             {
-                numberOfRoots = 0;
+                eqData->numberOfRoots = 0;
             }
             
         } else if (abs(a) < conditional_zero and abs(b) > conditional_zero) 
         {
-            rootsInformation[0] = -c/b;
-            numberOfRoots = 1;   
+            eqData->roots[0] = -c/b;
+            eqData->numberOfRoots = 1;   
             
         }  else if (abs(b) < conditional_zero and abs(a) < conditional_zero) 
         {
-            numberOfRoots = 0;
+            eqData->numberOfRoots = 0;
         }
-        rootsInformation[2] = numberOfRoots;
     
 }
 
-void outputValues(float* rootsInformation)
+void outputValues(equationData* eqData)     //Функция вывода решений уравнения//
 {
-    if (rootsInformation[2] > 0)
+    if (eqData->numberOfRoots > 0)
     {
        std::cout << "Корни уравнения:" << std::endl;
-        for (int i = 0; i < rootsInformation[2]; i++)
+        for (int i = 0; i < eqData->numberOfRoots; i++) //Извлекаем корни уравнения из массива с помощью цикла, если они есть//
         {
-         std::cout<< rootsInformation[i] << "   "<<std::endl;
-        } 
+         std::cout<< eqData->roots[i] << "   "<<std::endl;
+        }
     } else
     {
         std::cout<< "Действительных корней нет"<< std::endl;
-        
     }
-    
 }
 
 int main()
 {
-        float inputDataArray[3] = {0,0,0};
-        float rootsInformation[3] = {0, 0, 0};
-        inputData(inputDataArray);
-        solutionOfEquation(inputDataArray, rootsInformation);
-        outputValues(rootsInformation);
+    
+        equationData eqData_1;          //Создаём экземпляр структуры//
+        inputData(&eqData_1);           //Производим ввод данных и сохраняем их в атрибутах структуры//
+        solutionOfEquation(&eqData_1);  //Решаем уравнение//
+        outputValues(&eqData_1);        //Выводим решения в консоль//
         return 0;
 }
