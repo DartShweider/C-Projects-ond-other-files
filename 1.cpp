1aeaf64b91b6394f7f9971e84b102f2cfec64a0f
Dyomkin Dmitry <34719599+DartShweider@users.noreply.github.com>
2018-09-14 13:14:35 +0300

    Add files via upload
    Решение квадратных уравнений с использованием указателей

--- 1.cpp
+++ 1.cpp
@@ -2,65 +2,87 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
+void inputData(float* inputDataArray)
 {
+    float a, b, c;
+    
         
-        float a, b, c;
-        float conditional_zero = 0.00001;
-        
-        std::cout<< "Уравнение вида ax^2 + bx + c = 0. Введите параметры a, b, c. " << std::endl;
-        std::cout<< "Параметр а: ";
-        std::cin >> a;
-        std::cout<< "Параметр b: ";
-        std::cin >> b;
-        std::cout<< "Параметр c: ";
-        std::cin >> c;
-        
+    std::cout<< "Уравнение вида ax^2 + bx + c = 0. Введите параметры a, b, c. " << std::endl;
+    std::cout<< "Параметр а: ";
+    std::cin >> a;
+    std::cout<< "Параметр b: ";
+    std::cin >> b;
+    std::cout<< "Параметр c: ";
+    std::cin >> c;
+    inputDataArray[0] = a;
+    inputDataArray[1] = b;
+    inputDataArray[2] = c;
+}
+
+void solutionOfEquation(float* inputDataArray, float* rootsInformation)
+{
+    float conditional_zero = 0.00001;
+    int numberOfRoots = 0;
+    float a = inputDataArray[0];
+    float b = inputDataArray[1];
+    float c = inputDataArray[2];
         
         if (abs(a) > conditional_zero and abs(b) > conditional_zero and abs(c) > conditional_zero)
         {
             float d = pow(b, 2) - 4*a*c;
-            std::cout <<"Дискриминант: " << d << std::endl;
+            
             if (d > conditional_zero) 
             {
-                std::cout<< "Корень_1: "<< (-b - sqrt(d))/(2*a)<< "  " << "Корень_2: " << (-b + sqrt(d))/(2*a) << std::endl;
+                rootsInformation[0] = (-b - sqrt(d))/(2*a);
+                rootsInformation[1] = (-b + sqrt(d))/(2*a);
+                numberOfRoots = 2;
+                
             } else if (abs(d) <= conditional_zero) 
             {
-                std::cout<< "Корень: "<< -b/2 << std::endl;
+                rootsInformation[0] = -b/2;
+                numberOfRoots = 1;
+                
             } else if (d < -conditional_zero)
             {
-                std::cout<< "Действительных корней нет" << std::endl;
+                numberOfRoots = 0;
             }
             
-        } else if (abs(a) < conditional_zero) 
+        } else if (abs(a) < conditional_zero and abs(b) > conditional_zero) 
         {
-            if (abs(b) > conditional_zero)
-            {
-                std::cout<< "Корень: "<< -c/b << std::endl;
-            } else 
-            {
-                std::cout<< " Действительных корней нет" << std::endl;
-            }
+            rootsInformation[0] = -c/b;
+            numberOfRoots = 1;   
             
-            
-        }  else if (abs(b) < conditional_zero) 
+        }  else if (abs(b) < conditional_zero and abs(a) < conditional_zero) 
         {
-            if (abs(a) > conditional_zero)
-            {
-                std::cout<<"Корень_1: "<< -sqrt(c/a)<< "  " << "Корень_2: " << sqrt(c/a) << std::endl;
-            } else 
-            {
-                std::cout<< " Действительных корней нет" << std::endl;
-            }
-        } else if (abs(c) < conditional_zero) 
-        {
-            if (abs(a) > conditional_zero)
-            {
-                std::cout<< "Корень_1: "<< -b/a << "  "<< "Корень_2: "<< 0 << std::endl;
-            } else 
-            {
-                std::cout << " Действительных корней нет" << std::endl;
-            }
+            numberOfRoots = 0;
         }
+        rootsInformation[2] = numberOfRoots;
+    
+}
+
+void outputValues(float* rootsInformation)
+{
+    if (rootsInformation[2] > 0)
+    {
+       std::cout << "Корни уравнения:" << std::endl;
+        for (int i = 0; i < rootsInformation[2]; i++)
+        {
+         std::cout<< rootsInformation[i] << "   "<<std::endl;
+        } 
+    } else
+    {
+        std::cout<< "Действительных корней нет"<< std::endl;
+        
+    }
+    
+}
+
+int main()
+{
+        float inputDataArray[3] = {0,0,0};
+        float rootsInformation[3] = {0, 0, 0};
+        inputData(inputDataArray);
+        solutionOfEquation(inputDataArray, rootsInformation);
+        outputValues(rootsInformation);
         return 0;
 }
